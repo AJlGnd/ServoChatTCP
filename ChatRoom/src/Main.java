f1489c6507b9dc28b8ab4433a2875eb07e2114af
Abdelrahman Malavi <52723306+AJlGnd@users.noreply.github.com>
2023-05-15 02:21:48 +0300

    Add files via upload

---
+++ ChatRoom/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        ServerSocket srv = new ServerSocket(9898);
+//        Socket cl = srv.accept();
+//        InputStreamReader in = new InputStreamReader(cl.getInputStream());
+//        BufferedReader bf = new BufferedReader(in);
+//        String str = bf.readLine();
+//        System.out.println(str+" Connected!!");
+//        while (true) {
+//        str = bf.readLine();
+//                if(!Objects.equals(str, "")) {
+//                    System.out.println(str);
+//                }
+//                }
+
+
+
+
+    }
+}
